2d3903af5f09713a6e649caa01c458be7bb1a3f3
Invitado <invitado@s5pc29.eisc.univalle.edu.co>
2019-07-12 11:37:16 -0500

    Nuevo archivo

--- 2019_07_12/temporal_var.c
+++ 2019_07_12/temporal_var.c
@@ -2,19 +2,17 @@
 
 int a;
 
-int suma(int x, int y){
-   int z;
-   z = x + y;
-   a = a + 6;
-   return z;
+int suma(int x, int y) {
+  int z;
+  z = x + y;
+  a = a + 6;
+  return z;
 }
-
 
 int main(int argc, char** argv) {
- a = 4;
- printf("Suma: %d\n,suma(4,5));
- printf("A: %d\n, a);
- return 0;
+  a = 4;
+  printf("Suma: %d\n",suma(4,5));
+  printf("A: %d\n",a);
+  return 0;
 }
-
 
